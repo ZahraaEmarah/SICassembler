b18f20e462c32372e105ef6db3196c31dc9ecc70
Jarvis <emarahzahraa@gmail.com>
2019-04-20 21:49:33 +0200

    ListFile added

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -6,22 +6,20 @@ import java.util.ArrayList;
 
 public class Controller {
 	
-	String Label[] = new String[1000];
-	int labelindex =0;
-	String opCode[] = new String[1000];
-	int opcodeindex =0;
-	String operands[] = new String[1000] ;
-	int operandsindex =0;
+	String Label[]    = new String[1000];
+	String opCode[]   = new String[1000];
+	String operands[] = new String[1000];
+	String ErrorArr[] = new String[5];
 	String wordsArr[] = {"","",""};
-	String lineArr[] = {"","","",""};
-	
+	String lineArr[]  =  {"","","",""};
 	String directivesList[] = {"start","end","byte","word","resw","resb","equ","org","base"};
-	
 	String opcodeList[] = {"RMO","LDR","STR","LDCH","STCH","ADD","SUB","ADDR","SUBR","COMP"
 			              ,"COMR","J","JEQ","JLT","JGT","TIX","TIXR"};
 	int i=0;
 	int count = 0;
 	int error = 0;
+	int index = 0;
+	int errorindex = 0;
 	
 	public void ReadFile()
 	{
@@ -81,6 +79,9 @@ public class Controller {
 	    	}
 			
 			reader.close();
+			ValidateInstruction(Label, opCode, operands);
+			
+			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -109,7 +110,7 @@ public class Controller {
 				error = 1;
 			}	
 			
-			for(int i=0; i<10; i++)
+			for(int i=0; i<index; i++)
 			{
 				System.out.println("Label Array "+Label[i]);
 				System.out.println("Opcode Array "+opCode[i]);
@@ -137,47 +138,78 @@ public class Controller {
 					
 	}
 	
-	public void InstructionHandler(int n, String wordarray[]) // Populates the Instructions ArrayList
+	public void InstructionHandler(int n, String wordarray[]) // Populates the Instructions Array
 	{
 		for(i=0; i<wordarray.length; i++)
 		{
-			if(i==0 && !wordarray[0].equals("^")) // we have a label
+			if(i==0) // we have a label
 			{
-				Label[labelindex] = wordarray[0];
-				labelindex++;
-			}
+				Label[index] = wordarray[0];
+			} 
+			if(i==1) // we have an opcode
+			{
+				opCode[index] = wordarray[1];
+			} 
 			
-			if(i==1 && !wordarray[1].equals("^")) // we have an opcode
+			if(i==2) // we have operands
 			{
-				opCode[opcodeindex] = wordarray[1];
-				opcodeindex++;
-			}
+				operands[index] = wordarray[2];
+			} 
 			
-			if(i==2 && !wordarray[2].equals("^")) // we have operands
-			{
-				operands[operandsindex] = wordarray[2];
-				operandsindex++;
-			}
 		}
 		
-		ValidateOpcode(opCode);
-	}
-		
-	public void ValidateLabel(String label[])
-	{
-		
+		index++;
+		System.out.println(index);
 	}
 	
-	public void ValidateOpcode(String opcode[])
+	public void ValidateInstruction(String labelarr[], String opcodeArr[], String operandsArr[])
+	{
+		for(int i=0; i< index; i++)
+		{
+			if(i==0 && labelarr[0].equals("^"))
+			{
+				//error missing label
+			}
+			
+			ValidateLabel(labelarr[i]);
+			ValidateOpcode(opcodeArr[i]);
+			operandsArr[i]=ValidateOperands(operandsArr[i]);
+			
+			writeToFile(labelarr[i],opcodeArr[i],operandsArr[i], ErrorArr[i], i);
+		}
+	}
+		
+	
+	public void ValidateLabel(String label)
+	{
+		int duplicate = 0;
+		
+		for(int i=0; i< index;i++)
+		{
+			if(Label[i].compareTo("^") == 0)
+			{
+				Label[i] = "    ";  //replace all ^
+			}
+			else if(Label[i].compareTo(label) != 0)
+			{
+				duplicate = 1;
+				ErrorArr[errorindex] = "error [04] : 'duplicate label definition '";
+				errorindex++;
+			}
+			
+			//System.out.println("new label "+ Label[i]);
+		}
+	}
+	
+	
+	public void ValidateOpcode(String opcode)
 	{
 		int j=0;
-		int found=0;
-		while(opCode[j] != null)
-		{		
-			found=0;
+		int found=0;	
+		
 			for(int i=0; i<17; i++)
 			{
-				if(opCode[j].compareToIgnoreCase(opcodeList[i]) == 0)
+				if(opcode.compareToIgnoreCase(opcodeList[i]) == 0)
 				{
 					found = 1;
 				}
@@ -185,7 +217,7 @@ public class Controller {
 			
 			for(int i=0; i<9; i++)
 			{
-				if(opCode[j].compareToIgnoreCase(directivesList[i]) == 0)
+				if(opcode.compareToIgnoreCase(directivesList[i]) == 0)
 				{
 					found = 1;
 				}
@@ -193,16 +225,68 @@ public class Controller {
 			
 			if(found == 0)
 			{
-				System.out.println("error [08] : 'unrecognized operation code '");
+				ErrorArr[errorindex] = "error [08] : 'unrecognized operation code '";
+				errorindex++;
 			}
 			
-			j++;
-		}
 	}
 	
-	public void ValidateOperands(String operands[])
+	
+	public String ValidateOperands(String operands)
+	{
+		if(operands == "^")
+		 operands = "";
+		
+		return operands;
+	}
+	
+	
+	
+	public void writeToFile(String label, String opcode, String operands, String Error, int indx)
 	{
 		
+		String Inst;
+		if(Error != null)
+		{
+			  Inst = label +"      "+ opcode +"\t\t" + operands + "\t" + Error ;
+		} else 
+		{			
+			  Inst = label +"      "+ opcode +"\t\t" + operands + "\t"  ;
+		}
+	
+		 try
+		 { 
+			 BufferedWriter bw = new BufferedWriter(new FileWriter(new File("ListFile.txt"), true)); 
+			 System.out.println(Inst);
+	           
+	           if(indx == 0)
+	           {
+	        	   PrintWriter pw = new PrintWriter("ListFile.txt");
+	        	   pw.close();
+	        	   bw.write("  **** SIC Assembler ****"); 
+	        	   bw.newLine();bw.newLine();
+	        	   bw.write(Inst);
+	        	   bw.newLine();	        	   
+	           }	      
+	           else 
+	           {
+	        	   
+	        	   bw.write(Inst);
+	        	   bw.newLine();
+	           }
+	           
+	           if(indx+1 == index)
+	           {
+	        	  bw.newLine();
+	        	  bw.write("  **** END OF PASS 1 ****"); 
+	           }
+	           
+	           bw.close();
+	      }
+		 catch(Exception e)
+		 {
+			 e.printStackTrace();
+	     }
 	}
 	    
 }
